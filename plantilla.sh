8e4370a4dedab23a3cec5d0dca60b1ae3a7ab001
Raúl Caro Pastorino <tecnico@fryntiz.es>
2017-12-21 20:58:02 +0100

    Agregadas constantes

--- plantilla.sh
+++ plantilla.sh
@@ -8,3 +8,26 @@
 # @web        www.fryntiz.es
 # @github     https://github.com/fryntiz/
 #
+
+############################
+##     INSTRUCCIONES      ##
+############################
+
+############################
+##       CONSTANTES       ##
+############################
+amarillo="\033[1;33m"
+azul="\033[1;34m"
+blanco="\033[1;37m"
+cyan="\033[1;36m"
+gris="\033[0;37m"
+magenta="\033[1;35m"
+rojo="\033[1;31m"
+verde="\033[1;32m"
+
+WORKSCRIPT=''
+USER=$(whoami)
+
+
+
+exit 0

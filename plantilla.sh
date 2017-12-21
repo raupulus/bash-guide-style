370f721964ba7aa49b36260ce3af5e91febfeb7a
Raúl Caro Pastorino <tecnico@fryntiz.es>
2017-12-21 20:57:45 +0100

    Añadidos datos del autor

--- plantilla.sh
+++ plantilla.sh
@@ -1,2 +1,10 @@
 #!/usr/bin/env bash
 # -*- ENCODING: UTF-8 -*-
+#
+# @author     Raúl Caro Pastorino
+# @copyright  Copyright © 2017 Raúl Caro Pastorino
+# @license    https://wwww.gnu.org/licenses/gpl.txt
+# @email      tecnico@fryntiz.es
+# @web        www.fryntiz.es
+# @github     https://github.com/fryntiz/
+#

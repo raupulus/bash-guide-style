56df14ae545e356d001f9174bfc654f66de6477e
Raúl Caro Pastorino <tecnico@fryntiz.es>
2017-12-22 00:04:26 +0100

    Adaptados comentarios a las reglas

--- plantilla.sh
+++ plantilla.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 # -*- ENCODING: UTF-8 -*-
-#
-# @author     Raúl Caro Pastorino
-# @copyright  Copyright © 2017 Raúl Caro Pastorino
-# @license    https://wwww.gnu.org/licenses/gpl.txt
-# @email      tecnico@fryntiz.es
-# @web        www.fryntiz.es
-# @github     https://github.com/fryntiz
-# @gitlab     https://gitlab.com/fryntiz
-#
-#             Guía de estilos aplicada:
-# @style      https://github.com/fryntiz/Bash_Style_Guide
+##
+## @author     Raúl Caro Pastorino
+## @copyright  Copyright © 2017 Raúl Caro Pastorino
+## @license    https://wwww.gnu.org/licenses/gpl.txt
+## @email      tecnico@fryntiz.es
+## @web        www.fryntiz.es
+## @github     https://github.com/fryntiz
+## @gitlab     https://gitlab.com/fryntiz
+##
+##             Guía de estilos aplicada:
+## @style      https://github.com/fryntiz/Bash_Style_Guide
 
 ############################
 ##     INSTRUCCIONES      ##
@@ -19,17 +19,17 @@
 ############################
 ##       CONSTANTES       ##
 ############################
-AM="\033[1;33m" # Color Amarillo
-AZ="\033[1;34m" # Color Azul
-BL="\033[1;37m" # Color Blanco
-CY="\033[1;36m" # Color Cyan
-GR="\033[0;37m" # Color Gris
-MA="\033[1;35m" # Color Magenta
-RO="\033[1;31m" # Color Rojo
-VE="\033[1;32m" # Color Verde
+AM="\033[1;33m"  ## Color Amarillo
+AZ="\033[1;34m"  ## Color Azul
+BL="\033[1;37m"  ## Color Blanco
+CY="\033[1;36m"  ## Color Cyan
+GR="\033[0;37m"  ## Color Gris
+MA="\033[1;35m"  ## Color Magenta
+RO="\033[1;31m"  ## Color Rojo
+VE="\033[1;32m"  ## Color Verde
 
-WORKSCRIPT=$(pwd) # Directorio principal del script
-USER=$(whoami) # Usuario que ejecuta el script
+WORKSCRIPT=$(pwd)  ## Directorio principal del script
+USER=$(whoami)     ## Usuario que ejecuta el script
 
 ###########################
 ##       VARIABLES       ##
